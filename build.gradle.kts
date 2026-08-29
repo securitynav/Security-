87949b57a018c1664f1751f7663d014b5787a912
miappnav-cell <miappnav@gmail.com>
2026-08-29 12:59:20 -0400

    fix: separar correctamente pluginManagement y dependencyResolutionManagement hacia settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,3 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = java.net.URI("https://jitpack.io") }
-    }
-}
-
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
