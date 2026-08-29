e75f6410aa383eb0e43b6f6d375a71021ffa7bc8
miappnav-cell <miappnav@gmail.com>
2026-08-29 11:56:43 -0400

    fix: configurar repositorios en settings.gradle.kts y crear MainActivity y layouts faltantes

---
+++ app/src/main/java/com/securitynav/security/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.securitynav.security
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pin)
+    }
+}
