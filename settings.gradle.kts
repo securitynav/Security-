eb398998abd4b7f3f0fce26a5211d1dc179d2d28
Eldriverhd <eldriverrd7@gmail.com>
2026-08-30 13:26:12 -0400

    ci: ensure mavenCentral and jitpack repositories for dependencies (blurview)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,17 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = java.net.URI("https://jitpack.io") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
