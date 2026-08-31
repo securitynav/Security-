2fc11804d75c3287dcf96efd78c868d882a83df4
Empryrd <bivianrow@gmail.com>
2026-08-31 09:12:17 -0400

    fix: correct BlurView package import path to com.github.dimezis (lowercase)

--- app/src/main/java/com/securitynav/security/ui/MainActivity.kt
+++ app/src/main/java/com/securitynav/security/ui/MainActivity.kt
@@ -15,8 +15,8 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import com.airbnb.lottie.LottieCompositionFactory
-import com.github.Dimezis.blurview.RenderScriptBlur
-import com.github.Dimezis.blurview.BlurView
+import com.github.dimezis.blurview.RenderScriptBlur
+import com.github.dimezis.blurview.BlurView
 import com.securitynav.security.R
 import com.securitynav.security.data.AuthManager
 import com.securitynav.security.databinding.ActivityMainBinding
--- app/src/main/java/com/securitynav/security/ui/utils/BlurHelper.kt
+++ app/src/main/java/com/securitynav/security/ui/utils/BlurHelper.kt
@@ -1,8 +1,8 @@
 package com.securitynav.security.ui.utils
 
 import android.view.View
-import com.github.Dimezis.blurview.BlurView
-import com.github.Dimezis.blurview.RenderScriptBlur
+import com.github.dimezis.blurview.BlurView
+import com.github.dimezis.blurview.RenderScriptBlur
 
 object BlurHelper {
     fun setupBlur(blurView: BlurView, rootView: View) {
