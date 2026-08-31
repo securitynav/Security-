63b2f1c0b25b95685d59fce8714e3dcc9ca454c4
miappnav-cell <miappnav@gmail.com>
2026-08-31 08:20:40 -0400

    Fix: Agregar repositorio JitPack y dependencia BlurView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,6 @@ dependencies {
 
     // UI Enhancements
     implementation("com.airbnb.android:lottie:6.4.0")
-    implementation("com.github.Dimezis:BlurView:version-2.0.6")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
@@ -92,4 +91,4 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
-dependencies { implementation("com.github.Dimezis:BlurView:version-2.0.6") }
+implementation("com.github.Dimezis:BlurView:version-2.0.3")
