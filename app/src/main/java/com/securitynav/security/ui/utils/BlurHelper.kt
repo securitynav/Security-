5dc069e20198304d32d2e691c50ab8364bc4062e
miappnav-cell <miappnav@gmail.com>
2026-08-31 10:22:23 -0400

    Fix: Corregir paquetes de BlurView e integrar binding en RegisterActivity

--- app/src/main/java/com/securitynav/security/ui/utils/BlurHelper.kt
+++ app/src/main/java/com/securitynav/security/ui/utils/BlurHelper.kt
@@ -1,8 +1,8 @@
 package com.securitynav.security.ui.utils
 
 import android.view.View
-import com.github.dimezis.blurview.BlurView
-import com.github.dimezis.blurview.RenderScriptBlur
+import eightbitlab.com.blurview.blurview.BlurView
+import eightbitlab.com.blurview.blurview.RenderScriptBlur
 
 object BlurHelper {
     fun setupBlur(blurView: BlurView, rootView: View) {
